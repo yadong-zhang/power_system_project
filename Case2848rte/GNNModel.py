a0b064414c71dccd9036084d039923d2b8701eec
Yadong Zhang <yadong.zhang@vanderbilt.edu>
2024-04-16 11:43:03 -0500

    Train model

--- Case2848rte/GNNModel.py
+++ Case2848rte/GNNModel.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8eeafce4b9911a256cd9a61ad88cf89b8c4e4cd8bb188960b2f443f7f1518ed0
-size 1591
+oid sha256:71761640bad661f3e3dd06edf9cf4ff6fef9574e4c5e21c9e9b9d02cefcb180a
+size 1410
--- Case2848rte/config.py
+++ Case2848rte/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18820e17b14acea6b47ae53be66790ea22322bf960fb6ccdf49121cd1f5bbd39
-size 22
+oid sha256:8ce58d730259656b3e930c16017ecdeaadb892df1dc3d15f5d8defb6de3cdcfb
+size 18
